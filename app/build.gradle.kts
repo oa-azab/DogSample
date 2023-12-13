f3189b54e2a63513a98c0d9791131f7177b55bc8
Omar Ahmed <oa.azab@gmail.com>
2023-12-13 21:49:02 +0200

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -41,6 +42,26 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Dagger
+    implementation("com.google.dagger:dagger:2.49")
+    kapt("com.google.dagger:dagger-compiler:2.49")
+
+    // Dagger Android
+    implementation("com.google.dagger:dagger-android:2.49")
+    implementation("com.google.dagger:dagger-android-support:2.49")
+    kapt("com.google.dagger:dagger-android-processor:2.49")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
