a9ca84532510309c5d770415d89037c6e115725d
Omar Ahmed <oa.azab@gmail.com>
2023-12-15 06:14:51 +0200

    refactor DogService

--- app/src/main/java/com/omarahmed/dogsample/data/DogService.kt
+++ app/src/main/java/com/omarahmed/dogsample/data/DogService.kt
@@ -2,12 +2,26 @@ package com.omarahmed.dogsample.data
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface DogService {
 
-    @Headers("x-api-key: live_YJo8baAjOk1U86prlu1IdWZn6t8Xu7x1NQj6zxqEwIHKAVSkrLTEb6uS9LEfUo0E")
-    @GET("v1/images/search?limit=50&has_breeds=true&size=small")
-    suspend fun getAll(): Response<List<RemoteDog>>
+    companion object {
+        private const val API_KEY =
+            "live_YJo8baAjOk1U86prlu1IdWZn6t8Xu7x1NQj6zxqEwIHKAVSkrLTEb6uS9LEfUo0E"
+        private const val DEFAULT_LIMIT = 50
+        private const val DEFAULT_HAS_BREEDS = true
+        private const val DEFAULT_IMAGE_SIZE = "small"
+    }
+
+
+    @GET("v1/images/search")
+    suspend fun getAll(
+        @Header("x-api-key") apiKey: String = API_KEY,
+        @Query("limit") limit: Int = DEFAULT_LIMIT,
+        @Query("has_breeds") hasBreeds: Boolean = DEFAULT_HAS_BREEDS,
+        @Query("size") imageSize: String = DEFAULT_IMAGE_SIZE,
+    ): Response<List<RemoteDog>>
 
 }
